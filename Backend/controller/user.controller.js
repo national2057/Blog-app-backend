96c557091b9a1a5d56e7de156c197636b85e55a0
national2057 <nationalshrestha@gmail.com>
2025-01-27 13:43:46 +0545

    authentication done

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,50 +1,9 @@
-// const mongoose = require("mongoose");
-// const { User } = require("../models/user.model");
-
-// exports.registerUser = async (req, res) => {
-//   try {
-//     const { fullName, email, password } = req.body;
-//     await User.create({
-//       fullName,
-//       email,
-//       password,
-//     });
-//     return res.status(200).json({
-//       message: "User register successfully.",
-//       success: true,
-//     });
-//   } catch (error) {
-//    console.log(error);
-//    return res.status(404).json({
-//       message: "Failed to register User",
-//       success: false,
-//    });
-//   };
-// };
-
-// exports.loginUser = async (req, res) => {
-//    try {
-//       const {email, password} = req.body;
-//       const token = await User.matchPasswordAndGenerateToken(  email, password );
-//       console.log("Token:", token);
-//       return res.cookie("Token", token).status(200).json({
-//          message: "User logged in successfully.",
-//          success: true,
-//          token,
-//       });
-//    } catch (error) {
-//       console.log(error);
-//       return res.status(400).json({
-//          message: "Failed to login user.",
-//          success: false,
-//       });
-//    };
-// };
-
 const mongoose = require("mongoose");
 const { User } = require("../models/user.model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+// Register..
 exports.registerUser = async (req, res) => {
   try {
     const { fullName, email, password } = req.body;
@@ -65,7 +24,7 @@ exports.registerUser = async (req, res) => {
     const newUser = await User.create({
       fullName,
       email,
-      password,
+      password: hashedPassword,
     });
     return res.status(201).json({
       message: "User registered successfully!!",
@@ -82,6 +41,7 @@ exports.registerUser = async (req, res) => {
   }
 };
 
+// Login..
 exports.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -92,7 +52,7 @@ exports.loginUser = async (req, res) => {
         error: error.message,
       });
     }
-    const user = await User.findOne({ email });
+    let user = await User.findOne({ email });
     if (!user) {
       return res.status(404).json({
         message: "User not Found!!",
@@ -119,7 +79,6 @@ exports.loginUser = async (req, res) => {
       _id: user._id,
       fullName: user.fullName,
       email: user.email,
-      password: user.password,
     };
     return res
       .status(200)
@@ -142,79 +101,7 @@ exports.loginUser = async (req, res) => {
   }
 };
 
-// exports.loginUser = async (req, res) => {
-//   try {
-//     const { email, password, role } = req.body;
-//     if (!email || !password || !role) {
-//       return res.status(400).json({
-//         message: "Something is missing!!",
-//         success: false,
-//         error: error.message,
-//       });
-//     }
-//     let user = await User.findOne({ email });
-//     if (!user) {
-//       return res.status(404).status({
-//         message: "User not found.",
-//         success: false,
-//         error: error.message,
-//       });
-//     }
-//     const isPasswordMatch = await bcrypt.compare(password, user.password);
-//     if (!isPasswordMatch) {
-//       return res.status(400).status({
-//         message: "Incorrect password.",
-//         success: false,
-//         error: error.message,
-//       });
-//     }
-
-//     // check role is correct or not..
-//     if (role !== user.role) {
-//       return res.status(400).json({
-//         message: "Account doesn't exist with current role.",
-//         success: false,
-//         error: error.message,
-//       });
-//     }
-
-//     // token generate..
-//     const tokenData = {
-//       userId: user._id,
-//     };
-//     const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
-//       expiresIn: "1d",
-//     });
-
-//     user = {
-//       _id: user._id,
-//       fullName: user.fullName,
-//       email: user.email,
-//       password: user.password,
-//     };
-
-//     return res
-//       .status(200)
-//       .cookie("token", token, {
-//         maxAge: 1 * 24 * 60 * 60 * 1000,
-//         httpOnly: true,
-//         sameSite: "strict",
-//       })
-//       .json({
-//         message: `Welcome ${user.fullName}`,
-//         success: true,
-//         user,
-//       });
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json({
-//       message: "Internal server error.",
-//       success: false,
-//     });
-//   }
-// };
-
-// Logout
+// Logout..
 exports.logoutUser = async (req, res) => {
   try {
     return res
